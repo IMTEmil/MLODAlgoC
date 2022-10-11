86e6a04a7336248da7a9641be771ee609f40ee01
Emil Pérouse <=>
2022-10-11 17:30:07 +0200

    add file for second exercise

---
+++ TP-02-pointeurs/02-matrices/matrices.c
@@ -0,0 +1,8 @@
+#include "matrices.h"
+
+void matrix_mult(int64_t **MatriceRes, int64_t **Matrice1, int64_t **Matrice2)
+{
+    int i = 0;
+    int j = 0;
+    
+}
---
+++ TP-02-pointeurs/02-matrices/matrices.h
@@ -0,0 +1,8 @@
+#ifndef _MATRICES_H_
+#define _MATRICES_H_
+
+void matrix_mult(int64_t **MatriceRes, int64_t **Matrice1, int64_t **Matrice2);
+
+void matrix_print(int64_t **matrice);
+
+#endif /* _MATRICES_H_ */
